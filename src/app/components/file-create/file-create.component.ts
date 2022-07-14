0cd216cbd9679e1303d9ddc888f5fac23b12446f
nieswiezyowoc17135 <66435218+nieswiezyowoc17135@users.noreply.github.com>
2022-07-14 15:57:43 +0200

    Prawie ogarnięte tworzenie pliku

--- src/app/components/file-create/file-create.component.ts
+++ src/app/components/file-create/file-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { File } from 'src/app/models/file';
+import { FileService } from 'src/app/services/file.service';
 
 @Component({
   selector: 'app-file-create',
@@ -7,11 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FileCreateComponent implements OnInit {
 
-  
+  fileData: File = {skip: 0, take: 0, word:''};
 
-  constructor() { }
+  constructor(private fileService:FileService) { }
 
   ngOnInit(): void {
+  }
+
+  generateFile() {
+    this.fileService.createFile(this.fileData.skip, this.fileData.take, this.fileData.word).subscribe();
   }
 
 }
--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { File } from '../models/file';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class FileService {
 
-  constructor() { }
+  private fileUrl = 'https://localhost:7249/api/File?take='
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  createFile(skip: number, take: number, word: string) : Observable<File> {
+    return this.http.get<File>(this.fileUrl+take+'&skip='+skip+'&word='+word);
+  }
+
+  constructor(
+    private http: HttpClient
+  ) { }
 }
